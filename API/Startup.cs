632e5b928e66fabe68d088c6faf21b20250d7ac7
Alex Gale <alexrobertgale@gmail.com>
2022-11-15 23:47:28 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ services.AddDbContext<DataContext>(options =>
 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 });
             services.AddControllers();
+           services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -55,6 +56,8 @@ options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
